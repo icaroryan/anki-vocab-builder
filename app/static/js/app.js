d1d0a9bbdb0211ce9f9dbc6d7f60a96df0195eb5
Icaro Ryan <rfvb10hu@gmail.com>
2020-08-17 12:19:08 -0300

    Javascript to handle tabs

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,19 @@
-function open_page() {
-    var elmts = document.getElementsByClassName("tabContentContainer")
-    for (var i = 0; i < elmts.length; i +=1) {
-        elmts[i].style.display = "none";
+function open_page(btn) {
+    // Remove the class "active" from all Buttons and TabContent
+    var active_buttons = document.getElementsByClassName("active_button");
+    var active_contents = document.getElementsByClassName("active_content");
+
+    if (active_contents.length && active_buttons.length > 0) {
+        active_buttons[0].classList.remove("active_button");
+        active_contents[0].classList.remove("active_content");
     }
 
+    // Add the class "active" in the clicked button (change the button background)
+    document.getElementById(btn).classList.add("active_button");
+
+    // Add the class "active" in the respective tab_content, according to the clicked button (Show the specific content (display:block))
+    document.getElementById(btn + "_embed").classList.add("active_content")
     
 }
-
-// document.getElementsByClassName("tabButton").addEventListener("click", open_page())
+// Click on the default tab
+document.getElementsByClassName("tabButton")[0].click();
