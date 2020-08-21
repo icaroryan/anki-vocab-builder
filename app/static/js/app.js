8f57623f8260be6f47f5ae3187a7d43420d02f68
Icaro Ryan <rfvb10hu@gmail.com>
2020-08-21 11:42:48 -0300

    Submit by enter key and check if all field are not empty

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -26,6 +26,19 @@ document.getElementById("icon").addEventListener("click", function () {
     form.submit();
 })
 
+var input_word = document.getElementById("word");
+var input_select = document.getElementById("dictionaries")
+document.addEventListener("keypress", function() {
+    if (event.keyCode == 13) {
+        if (input_word.value.length != 0 && input_select.value != "") {
+            form.submit();
+        }
+        else {
+            alert("You should fill all the fields");
+        }
+    }
+})
+
 
 // Click on the default tab
 document.getElementsByClassName("tabButton")[0].click();
--- app/views.py
+++ app/views.py
@@ -5,11 +5,22 @@ from flask import request, render_template, flash, redirect, url_for
 # from urllib.parse import unquote
 # import re
 
+# dictionaries = [
+#     {'name': 'Cambridge Dictionary', 'id': 'cambridge', 'url': 'https://dictionary.cambridge.org/us/search/direct/?datasetsearch=english&q={{ word }}', 'x-frame-bypass': False},
+#     {'name': 'Longman Dictionary', 'id': 'longman', 'url': 'https://www.ldoceonline.com/search/english/direct/?datasetsearch=english&q={{ word }}', 'x-frame-bypass': True},
+#     {'name': 'Thesaurus Synonyms', 'id': 'thesaurus', 'url': 'https://www.thesaurus.com/browse/{{ word }}', 'x-frame-bypass': False},
+#     {'name': 'DuckDuckGo Images', 'id': 'duckduckgo', 'url': 'https://duckduckgo.com/?q={{ word }}&iax=images&ia=images', 'x-frame-bypass': True},
+#     {'name': 'Sentence Dictionary', 'id': 'sentencedict', 'url': 'https://sentencedict.com/', 'x-frame-bypass': False},
+#     {'name': 'Macmillan Dictionary', 'id': 'macmillan', 'url': 'https://www.macmillandictionary.com/search/british/direct/?q={{ word }}', 'x-frame-bypass': True}
+# ]
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
 
         word = request.form["word"].lower()
+
+        listDictionaries = request.form.getlist("dictionaries")
 
         return redirect(url_for("search", word=word))
         
